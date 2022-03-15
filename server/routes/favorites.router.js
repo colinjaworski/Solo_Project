1188904d691484137bde745475f59aba2a5135f3
colin jaworski <colinrjaworski.84@gmail.com>
2022-03-15 16:16:26 -0500

    created favorites router

---
+++ server/routes/favorites.router.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const pool = require('../modules/pool');
+const router = express.Router();
+
+/**
+ * GET * favorite trees for user
+ */
+router.get('/', (req, res) => {
+  // GET route code here
+});
+
+/**
+ * POST route template
+ */
+router.post('/', (req, res) => {
+  // POST route code here
+});
+
+module.exports = router;
