5cae5b3309f96ef74d43595785ebaca9430cddeb
colin jaworski <colinrjaworski.84@gmail.com>
2022-03-16 07:59:02 -0500

    added favoritesSaga, updated rootSaga.

---
+++ src/redux/sagas/favoritesSaga.js
@@ -0,0 +1,24 @@
+import { put, take, takeLatest } from 'redux-saga/effects';
+import axios from 'axios';
+
+
+
+
+function* getFavorites() {
+
+    try {
+        const response = yield axios.get('/api/favorites');
+        yield put({ type: 'SET_SHELF', payload: response.data }); // is SET_SHELF right?
+
+
+    } catch (error) {
+        console.log('SET favorites failed', error);
+
+    }
+}
+
+function* favoritesSaga() {
+    yield takeLatest('FETCH_SHELF', getFavorites); // is FETCH_SHELF right? Is it supposed to match the getFavorites?
+}
+
+export default favoritesSaga;
