1facc191db577d6ff04fea8cfe5103ae0c8197ca
colin jaworski <colinrjaworski.84@gmail.com>
2022-03-17 15:52:47 -0500

    get request now is specific and only grabs data which satisfys input fields.

--- server/routes/search.router.js
+++ server/routes/search.router.js
@@ -9,23 +9,22 @@ router.get(`/:maxHeight/:maxWidth/`, (req, res) => {
 
     console.log(`in router.get, search router, max height: ${req.params.maxHeight}, max width: ${req.params.maxWidth}`)
 
-    // if (req.isAuthenticated()) {
-    //     const queryText = `SELECT *
-    //     FROM "recommended_southEast_deciduous"
-    //     WHERE "height" <= $1 AND "width" <= $2;`;
+    if (req.isAuthenticated()) {
+        const queryText = `SELECT * FROM "recommended_southEast_deciduous" 
+        WHERE "height" <= $1 AND "width" <= $2`;
 
-    //     pool.query(queryText[req.params.maxHeight, req.params.maxWidth])
+        pool.query(queryText, [req.params.maxHeight, req.params.maxWidth])
 
-    //         .then((result) => {
-    //             res.send(result.rows)
-    //         })
-    //         .catch((error) => {
-    //             console.log(error);
-    //             res.sendStatus(500);
-    //         })
-    // } else {
-    //     res.sendStatus(403);
-    // }
+            .then((result) => {
+                res.send(result.rows)
+            })
+            .catch((error) => {
+                console.log(error);
+                res.sendStatus(500);
+            })
+    } else {
+        res.sendStatus(403);
+    }
 });
 
 /**
